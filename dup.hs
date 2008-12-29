b85697d7175e410f198f548289a7ce9e368e7e98
Francisco Ferreira <fco@yaess.com>
2008-12-29 02:21:04 -0500

    VERY preliminar support for content based duplicates added, not really working

--- dup.hs
+++ dup.hs
@@ -17,12 +17,16 @@
 --
 
 import Control.Monad (forM)
-import Control.Exception (handle)
+import Control.Exception (handle, bracket)
 import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>), splitFileName)
 import System.Environment (getArgs)
 import System.Posix.Files (getSymbolicLinkStatus, isSymbolicLink)
-import Data.List (sort, group)
+import Data.List (sort, sortBy, group, groupBy)
+import System.IO (openFile, IOMode(..), hClose, hFileSize)
+import qualified Data.ByteString.Lazy as D
+
+import MD5
 
 getRecursiveContents :: FilePath -> IO[FilePath]
 getRecursiveContents topdir = do
@@ -37,6 +41,18 @@ getRecursiveContents topdir = do
       else return [path]
   return (concat paths)
 
+fileNameAndSize :: FilePath -> IO (FilePath, Integer)
+fileNameAndSize path = handle (\_ -> return (path, 0)) $ do
+  bracket (openFile path ReadMode) hClose $ \h -> do
+    size <- hFileSize h
+    return (path, size)
+
+fileNameAndMD5 :: FilePath -> IO (FilePath, MD5Digest)
+fileNameAndMD5 path = do
+  chksum <- fileMD5 path
+  return (path, chksum)
+
+
 -- A safe version of getDirectoryContents that returns [] when
 -- the directory can't be opened
 getDirectoryContents' :: FilePath -> IO[FilePath]
@@ -45,14 +61,41 @@ getDirectoryContents' files = handle (\_ -> return []) $ do
 
 findDuplicates origin = do
   files <- getRecursiveContents origin 
-  return $ (filterDups .  map (snd . splitFileName)) files
+  filesAndSizes <- mapM fileNameAndSize files
+  let sizeDups  = (filter (\e -> ((snd e) > 0)) . filterDupsBy isEqualSize ordBySize) filesAndSizes
+  --putStrLn $ show sizeDups 
+  filesAndMD5s <- mapM fileNameAndMD5 (map fst sizeDups)
+  let dups = filterDupsBy isEqualSize ordBySize filesAndMD5s
+  let pdups = map fst dups
+  return $ unlines pdups
+
+
+
 
 -- filters duplicates in an orderable list
 -- TODO optimize and write a function that doesn't need to order the list
 filterDups :: (Eq a, Ord a) => [a] -> [a]
 filterDups = map head . filter ((>1) . length) . group . sort
 
+--filterDupsBy :: (Eq a, Ord a) => (a -> a -> Bool) -> (a -> a -> Ordering) -> [a] -> [a]
+filterDupsBy eq ord = concat . filter ((>1) . length) . (groupBy eq) . (sortBy ord)
+
+--isEqualSize :: (a, Integer) -> (a, Integer) -> Bool
+isEqualSize (_, size1) (_, size2) = size1 == size2
+
+--ordBySize :: (a, Integer) -> (a, Integer) -> Ordering 
+ordBySize (_, size1) (_, size2) = compare size1 size2
+
+fileMD5 f = do
+  content <- D.readFile f
+  return $ md5 content
+
+compareMD5 f1 f2 = do
+  m1 <- fileMD5 f1
+  m2 <- fileMD5 f2
+  return $ m1 == m2
+
 main = do
   args <- getArgs
   duplicates <- findDuplicates (args !! 0)
-  putStrLn $ unlines duplicates 
+  putStrLn $ duplicates 
